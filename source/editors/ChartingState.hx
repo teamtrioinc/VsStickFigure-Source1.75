3683f847e463c310afca8f1858a4d7c82ced5ad7
teamtrioinc <166570293+teamtrioinc@users.noreply.github.com>
2024-06-11 11:23:58 -0500

    Updated ChartingState.hx AGAIN
    i think it should compile now

--- source/editors/ChartingState.hx
+++ source/editors/ChartingState.hx
@@ -2189,7 +2189,8 @@ class ChartingState extends MusicBeatState
 		var st:Float = sectionStartTime();
 		var et:Float = st + (Conductor.stepCrochet * steps);
 
-		@:privateAccess {if (FlxG.save.data.chart_waveformInst) {
+		@:privateAccess {
+		{if (FlxG.save.data.chart_waveformInst) {
 			var sound:FlxSound = FlxG.sound.music;
 			if (sound._sound != null && sound._sound.__buffer != null) {
 				var bytes:Bytes = sound._sound.__buffer.data.toBytes();
@@ -2222,7 +2223,7 @@ class ChartingState extends MusicBeatState
 				);
 			}
 		}
-
+	}
 		// Draws
 		var gSize:Int = Std.int(GRID_SIZE * 8);
 		var hSize:Int = Std.int(gSize / 2);
