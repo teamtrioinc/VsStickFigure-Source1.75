137f253f2ac2f83e5efad45f74b9017f138c793c
teamtrioinc <166570293+teamtrioinc@users.noreply.github.com>
2024-06-13 17:58:36 -0500

    Update FreeplayState.hx

--- source/FreeplayState.hx
+++ source/FreeplayState.hx
@@ -88,8 +88,7 @@ class FreeplayState extends MusicBeatState
 				addSong(song[0], i, song[1], FlxColor.fromRGB(colors[0], colors[1], colors[2]));
 			}
 		}
-	}
-}         		
+
                 WeekData.loadTheFirstEnabledMod();
 
 		/*		//KIND OF BROKEN NOW AND ALSO PRETTY USELESS//
